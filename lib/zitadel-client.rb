5a5c1853f8fbe47ffbd982f4a7e44c8f43dce96f
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-04-10 14:10:39 +0300

    Added a custom inflection rule to ensure that the OIDC is loaded correctly

--- lib/zitadel-client.rb
+++ lib/zitadel-client.rb
@@ -1,8 +1,9 @@
 require 'zeitwerk'
 
 loader = Zeitwerk::Loader.new
+loader.inflector.inflect "oidc_service_api" => "OIDCServiceApi"
 loader.tag = File.basename(__FILE__, ".rb")
-loader.collapse("#{__dir__}/zitadel-client/utils/")
+loader.collapse("#{__dir__}/zitadel-client/api/")
 loader.collapse("#{__dir__}/zitadel-client/auth/")
 loader.collapse("#{__dir__}/zitadel-client/models/")
 loader.collapse("#{__dir__}/zitadel-client/utils/")
