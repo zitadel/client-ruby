d5b141c2c3a41ca3be20414a8f792734504a5a6e
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-04-23 08:43:49 +0300

    Tweaked the minitest reporters to not use fileno and lineno in the testsuite attributes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,11 @@
 require 'dotenv'
 require 'simplecov'
 require 'simplecov-lcov'
+require 'warning'
+
 require_relative '../lib/zitadel_client'
 
+Warning.ignore(:method_redefined, __dir__)
 Dotenv.load('.env')
 
 # Configure the LCOV formatter
@@ -50,6 +53,51 @@ end
 
 begin
   require 'minitest/reporters'
+
+  module Minitest
+    module Reporters
+      # rubocop:disable Metrics/AbcSize,Style/Documentation,Metrics/MethodLength
+      class JUnitReporter
+        private
+
+        def parse_xml_for(xml, suite, tests)
+          suite_result = analyze_suite(tests)
+          file_path = get_relative_path(tests.first)
+
+          testsuite_attributes = {
+            name: suite,
+            file: file_path,
+            skipped: suite_result[:skip_count],
+            failures: suite_result[:fail_count],
+            errors: suite_result[:error_count],
+            tests: suite_result[:test_count],
+            assertions: suite_result[:assertion_count],
+            time: suite_result[:time]
+          }
+          testsuite_attributes[:timestamp] = suite_result[:timestamp] if @timestamp_report
+
+          xml.testsuite(testsuite_attributes) do
+            tests.each do |test|
+              line = get_source_location(test).last
+              xml.testcase(
+                name: test.name,
+                line: line,
+                classname: suite,
+                assertions: test.assertions,
+                time: test.time,
+                file: file_path
+              ) do
+                xml << xml_message_for(test) unless test.passed?
+                xml << xml_attachment_for(test) if test.respond_to?('metadata') && test.metadata[:failure_screenshot_path]
+              end
+            end
+          end
+        end
+      end
+      # rubocop:enable Metrics/AbcSize,Style/Documentation,Metrics/MethodLength
+    end
+  end
+
   unless ENV['RM_INFO']
     Minitest::Reporters.use! [
       Minitest::Reporters::SpecReporter.new(color: true),
